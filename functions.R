ce735e3b07b290a42fbd6b54594d56f8f95ce9d1
Wenceslao Arroyo-Machado <36030851+Wences91@users.noreply.github.com>
2021-11-28 20:44:20 +0100

    Update functions.R

--- functions.R
+++ functions.R
@@ -51,11 +51,16 @@ read_jcr <- function(path){
 
 biblio_cor <- function(df, indicator_1, indicator_2){
   
+  # To avodi functions errors
+  df <- as.data.frame(df)
+  
   df_cor <- data.frame(Category=unique(df$Category),
+                       p=NA,
                        Correlation=NA,
                        DB=NA,
                        Cat=NA,
                        Journals=NA,
+                       Docs=NA,
                        stringsAsFactors = FALSE)
   
   # NAs rows are removed
@@ -64,10 +69,14 @@ biblio_cor <- function(df, indicator_1, indicator_2){
   for(i in unique(df$Category)){
     df_cor[which(df_cor$Category==i),'Correlation'] <- cor(df[which(df$Category==i), indicator_1], df[which(df$Category==i), indicator_2], method='pearson')
     
+    try(df_cor[which(df_cor$Category==i),'p'] <- cor.test(df[which(df$Category==i), indicator_1],df[which(df$Category==i), indicator_2], conf.level=0.99)$p.value)
+    
     df_cor[which(df_cor$Category==i),'DB']  <- unique(df[which(df$Category==i), 'DB'])
     df_cor[which(df_cor$Category==i),'Cat'] <- unique(df[which(df$Category==i), 'Cat'])
     
     df_cor[which(df_cor$Category==i),'Journals'] <- dim(df[which(df$Category==i),])[1]
+    
+    df_cor[which(df_cor$Category==i),'Docs'] <- sum(df[which(df$Category==i), 'Total Articles'], na.rm = TRUE)
   }
   
   return(df_cor)
